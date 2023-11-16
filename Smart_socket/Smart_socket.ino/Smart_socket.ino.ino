c07ff168a1ed3e0cf7a2c5c55a7e850acaae611c
OleksandrLohun <oleksandrlogin2002@gmail.com>
2023-11-16 12:40:16 +0200

    OLED V1
    Added oled screen functionality

--- Smart_socket/Smart_socket.ino/Smart_socket.ino.ino
+++ Smart_socket/Smart_socket.ino/Smart_socket.ino.ino
@@ -1,4 +1,8 @@
+#include <Wire.h>
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSerif9pt7b.h>
 #include <EncButton.h>
 
 #define ENC_PIN_R D7
@@ -15,6 +19,11 @@
 #define OFF 1
 #endif
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 EncButton eb(ENC_PIN_R, ENC_PIN_L, ENC_PIN_BTN);
 
 bool state;
@@ -33,41 +42,35 @@ void encoder_setup()
   eb.setFastTimeout(30);
   eb.counter = 0;
 }
-
-void setup() 
-{
-    Serial.begin(SERIAL_SPD);
+void setup() {
+  Serial.begin(115200);
+ 
 
     state = OFF;
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, state);
 
     encoder_setup();
-}
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println("SSD1306 allocation failed");
+    for(;;);
+  }
+  delay(2000);
 
+  display.setFont(&FreeSerif9pt7b);
+  display.clearDisplay();
+  display.setTextSize(1);             
+  display.setTextColor(WHITE);        
+  display.setCursor(0,20);             
+  display.println("Hello, world!");
+  display.display();
+  delay(2000); 
+}
 void change_state()
 {
   state = !state;
   digitalWrite(LED_BUILTIN, state);
 }
-
-/*
-void pwm_led_up()
-{
-  if(state == 0){
-  britnes++;
-  analogWrite(LED_BUILTIN, britnes);
-  }
-}
-
-void pwm_led_dwn()
-{
-  if(state == 0){
-  britnes--;
-  analogWrite(LED_BUILTIN, britnes);
-  }
-}
-*/
 
 void encoder_handler()
 {
@@ -80,8 +83,7 @@ void encoder_handler()
 }
 
 
-void loop() 
-{
-  eb.tick();
+void loop() {
+   eb.tick();
   encoder_handler();
 }
