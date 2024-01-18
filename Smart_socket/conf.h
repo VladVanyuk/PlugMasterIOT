94bf71ead8b88d195fed0a5c334e8534496d074c
OleksandrLohun <oleksandrlogin2002@gmail.com>
2024-01-18 13:03:30 +0200

    Added light sleep mode to Vemos d1 mini

--- Smart_socket/conf.h
+++ Smart_socket/conf.h
@@ -4,5 +4,10 @@
 #define RELAY_SERVICE   1
 #define ENCODER_SERVICE 1
 #define OLED_SERVICE    1
-#define WIFI_SERVICE    1
+#define WIFI_SERVICE    0
 #define SENSOR_SERVICE 0
+#define LIGHTSLEEP_SERVICE 1
+
+
+
+
